3e4e17ee53f6da222dca50bcec48bbd397905055
ChemsCode <saidiceb@gmail.com>
2023-01-21 14:35:55 -0500

    added an about section to landing page

--- src/components/LandingComp/LandingPage.js
+++ src/components/LandingComp/LandingPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Hero from './Hero'
+import About from './About'
 
 function LandingPage() {
   return (
     <>
         <Navbar/>
         <Hero/>
+        <About/>
     </>
 
   )
